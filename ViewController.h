656ecbdcf94ec18c043ef0dc49347b381b40c96f
liuweili <weili.liu@kuwo.cn>
2017-05-19 13:32:53 +0800

    user2.

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by 刘威利 on 2017/5/5.
+//  Copyright © 2017年 lwl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ViewController.m
@@ -0,0 +1,39 @@
+//
+//  ViewController.m
+//  Demo
+//
+//  Created by 刘威利 on 2017/5/5.
+//  Copyright © 2017年 lwl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Do any additional setup after loading the view, typically from a nib.
+    NSString *bundlePath = [[NSBundle mainBundle] pathForResource:@"JuXingImage" ofType:@"bundle"];
+    NSBundle *myBundle = [NSBundle bundleWithPath:bundlePath];
+    NSLog(@"bundle = %@",myBundle);
+    
+    
+//    NSString *gifLopath = [myBundle pathForResource:@"feather_gold" ofType:@"png"];
+//    NSLog(@"path = %@",gifLopath);
+//    UIImage *image = [UIImage imageNamed:@"JuXingImage.bundle/feather_gold.png"];
+//    NSLog(@"image = %@",image);
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
