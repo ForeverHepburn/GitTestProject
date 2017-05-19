dd3b56a0c51e7bbb3e3792135838adc58589f7e6
liuweili <weili.liu@kuwo.cn>
2017-05-19 13:37:44 +0800

    fixed error.

--- AppDelegate.m
+++ AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    There is an error here, so i fixed it.
     return YES;
 }
 
